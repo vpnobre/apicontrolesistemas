3164160da98ec5ebfe1fe6e4961aee0b24b2df0f
Vicente Nobre <vpnobre@gmail.com>
2017-09-07 19:54:29 -0300

    insert dos dados

---
+++ controleAcesso/src/main/resources/META-INF/dados.sql
@@ -0,0 +1,3 @@
+INSERT INTO tb_usuario VALUES (1, true, 'adm', 'ROLE_ADM', 'e10adc3949ba59abbe56e057f20f883e');
+INSERT INTO tb_usuario VALUES (2, true, 'vicente', 'ROLE_ADM', 'e10adc3949ba59abbe56e057f20f883e');
+commit;
